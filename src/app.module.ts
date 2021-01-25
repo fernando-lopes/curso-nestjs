7b0238d722ce43ae448e9d71f7a1e7a0aaf21f32
Fernando Lopes <fernandolopes@live.com>
2021-01-25 09:06:24 -0300

    feat: adicionado typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
-
+import { StoreModule } from './modules/store/store.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://admin:petshop.123@cluster0.vbrfk.mongodb.net/petshop?retryWrites=true&w=majority',
     ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'curso.123',
+      database: 'petshop',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
     BackofficeModule,
+    StoreModule,
   ],
   controllers: [],
   providers: [],
